26da2dc819602f2cf9f46743ece85b29bf553daa
Emmanuel Neri <ddark.emanu@gmail.com>
2013-09-24 20:33:14 -0300

    criando uma criteria basica

---
+++ hibernate-exemplos/src/main/java/br/com/emanu/neri/blog/hibernate/CriteriaBasica.java
@@ -0,0 +1,45 @@
+package br.com.emanu.neri.blog.hibernate;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+public class CriteriaBasica {
+	
+	// injetando entityManager com as configurações do persistence.xml
+	@PersistenceContext(unitName = "HibernateExemplosPU") 
+	private EntityManager entityManager;                  
+	
+	/*
+	 * Consulta = Select * from entidade
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Entidade> criteriaBasica() {
+		// cria uma sessão do hibernate a partir do entityManager
+		Session session = (Session) this.entityManager.getDelegate(); 
+		
+		//A sessão do Hibernate possui o método createCriateria , que retorna
+		// uma critéria da entidade que voce deseja fazer a consulta
+		Criteria criteria = session.createCriteria(Entidade.class);
+		return criteria.list();  
+	}
+	
+	/*
+	 * Consulta = Select * from entidade where id = 1
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Entidade> criteriaBuscarPorId() {
+		Session session = (Session) this.entityManager.getDelegate(); 
+		Criteria criteria = session.createCriteria(Entidade.class);
+		
+		// cria uma restrição (where no sql) que seja igual ao valor vindo por parametro; 
+		criteria.add(Restrictions.eq("id", 1L));
+		return criteria.list();  
+	}
+
+}
---
+++ hibernate-exemplos/src/main/java/br/com/emanu/neri/blog/hibernate/Entidade.java
@@ -0,0 +1,26 @@
+package br.com.emanu.neri.blog.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Entidade {
+	
+	@Id
+	private Long id;
+	
+	private String nome;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
