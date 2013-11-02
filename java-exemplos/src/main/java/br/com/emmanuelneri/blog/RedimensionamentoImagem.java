2b6257e1899bde6059a557af20c54c7cb2fe4196
Emmanuel Neri <emmanuelneri@MacBook-Pro-de-Emmanuel.local>
2013-11-02 13:54:49 -0200

    Redimensionando de Imagens Proporcionalmente

---
+++ java-exemplos/src/main/java/br/com/emmanuelneri/blog/RedimensionamentoImagem.java
@@ -0,0 +1,22 @@
+package br.com.emmanuelneri.blog;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class RedimensionamentoImagem {
+
+    private static int tamanhoMaximo = 800;
+
+    public static void redimecionarImagem(InputStream inputStream, String caminhoDestino) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(inputStream);
+        Image thumbnail = bufferedImage.getScaledInstance(bufferedImage.getWidth() < 800 ? bufferedImage.getWidth() : tamanhoMaximo, -1, Image.SCALE_SMOOTH);
+        BufferedImage bufferedThumbnail = new BufferedImage(thumbnail.getWidth(null), thumbnail.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        bufferedThumbnail.getGraphics().drawImage(thumbnail, 0, 0, null);
+        ImageIO.write(bufferedThumbnail, "jpeg", new File(caminhoDestino));
+    }
+}
---
+++ java-exemplos/src/test/java/br/com/emmanuelneri/blog/RedimensionamentoImagemTest.java
@@ -0,0 +1,20 @@
+package br.com.emmanuelneri.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.*;
+
+@RunWith(JUnit4.class)
+public class RedimensionamentoImagemTest {
+
+    @Test
+    public void redimensionar() throws IOException {
+        File arquivo = new File("/Users/emmanuelneri/Desktop/imagem/original/foto.jpg");
+        InputStream inputStream = new FileInputStream(arquivo);
+
+       RedimensionamentoImagem.redimecionarImagem(inputStream, "/Users/emmanuelneri/Desktop/imagem/redimensionada/foto.jpg");
+    }
+
+}
