0f2960ea8f660acce0c0fb2f1d7031684d3d295c
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2013-11-25 23:03:23 -0200

    adicionado exemplos de equals e compareTO

--- guava-exemplos/src/main/java/butilities/GuavaObjects.java
+++ guava-exemplos/src/main/java/butilities/GuavaObjects.java
@@ -2,11 +2,14 @@ package butilities;
 
 
 import com.google.common.base.Objects;
+import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.Ordering;
 
-public class GuavaObjects {
+public class GuavaObjects implements Comparable<GuavaObjects> {
 
 	public static void main(String args[]) {
 		firstNonNul();
+		equals();
 	}
 
 	/*
@@ -20,4 +23,30 @@ public class GuavaObjects {
 		System.out.println(Objects.firstNonNull(numero, 0)); // 1
 	}
 
+	private static void equals() {
+		System.out.println(Objects.equal("a", "a")); //  true
+		System.out.println(Objects.equal(null, "a")); //  false
+		System.out.println(Objects.equal("a", null)); //  false
+		System.out.println(Objects.equal(null, null)); //  true
+	}
+
+
+	private String descricao;
+	private TipoObjects tipo;
+
+	@Override
+	public int compareTo(GuavaObjects that) {
+		return ComparisonChain.start()
+				.compare(this.descricao, that.descricao)
+				.compare(this.tipo, that.tipo, Ordering.natural().nullsLast())
+				.result();
+	}
+
+	enum TipoObjects {
+		CHECK,
+		VALIDADE
+		;
+	}
 }
+
+
