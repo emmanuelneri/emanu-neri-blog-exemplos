553883712db4e62a4d5ab6497b3a7524b6e1b42b
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2013-11-28 00:15:06 -0200

    adicionando map

--- guava-exemplos/src/main/java/collections/GuavaImmutableCollections.java
+++ guava-exemplos/src/main/java/collections/GuavaImmutableCollections.java
@@ -1,6 +1,7 @@
 package collections;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 public class GuavaImmutableCollections {
@@ -16,6 +17,9 @@ public class GuavaImmutableCollections {
 
 		ImmutableSet set = ImmutableSet.of("a", "b", "c", "c", "d", "z");  //[a, b, c, d, z]
 		System.out.println(set);
+
+		ImmutableMap map = ImmutableMap.of("1", 10, "2", 20, "3", 30);
+		System.out.println(map); // {1=10, 2=20, 3=30}
 	}
 
 	private static void convertendoColecoes() {
