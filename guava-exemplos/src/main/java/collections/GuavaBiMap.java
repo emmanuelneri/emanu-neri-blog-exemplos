56afaafe53f56eefb3e60050eff8a09fa64a15d9
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2013-11-30 20:37:35 -0200

    Guava Bimap

---
+++ guava-exemplos/src/main/java/collections/GuavaBiMap.java
@@ -0,0 +1,24 @@
+package collections;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multiset;
+
+public class GuavaBiMap {
+
+	public static void main(String args[]) {
+		criarBiMap();
+	}
+
+	private static void criarBiMap() {
+		BiMap<Integer, String> biMap = HashBiMap.create();
+		biMap.put(1, "Jose");
+		biMap.put(2, "Joao");
+
+		System.out.println(biMap.get(1)); // Jose
+		System.out.println(biMap.inverse().get("Joao")); // 2
+	}
+
+}
