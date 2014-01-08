30cd020283f67701f4994e7a4d9f0d1576ad89fe
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2014-01-07 22:46:03 -0200

    JodaTime YearMonth

---
+++ jodatime-exemplos/src/main/java/jodatime/JodaYearMonth.java
@@ -0,0 +1,38 @@
+package jodatime;
+
+import org.joda.time.YearMonth;
+
+import java.util.Date;
+
+public class JodaYearMonth {
+
+	private YearMonth yearMonthAtual;
+	private YearMonth yearMonthJaneiro2014;
+
+	public JodaYearMonth() {
+		this.yearMonthAtual = YearMonth.now();  // 2014 - 01
+		this.yearMonthJaneiro2014 = new YearMonth(2014, 1); // dia pode ser 1 ou 01
+
+		YearMonth yearMonthFromDate = new YearMonth().fromDateFields(new Date());
+	}
+
+	private void adicionar() {
+		this.yearMonthAtual.plusYears(1); // adiciona 1 ano
+		this.yearMonthAtual.plusMonths(3); // adiciona 3 meses
+	}
+
+	private void remover() {
+		this.yearMonthAtual.minusYears(2); // diminui 2 anos
+		this.yearMonthAtual.minusMonths(5); // diminui 5 meses
+	}
+
+	private void apresentacao() {
+		System.out.println(this.yearMonthJaneiro2014); // 2014-01
+		System.out.println(this.yearMonthJaneiro2014.getYear()); // 2014
+		System.out.println(this.yearMonthJaneiro2014.getMonthOfYear()); // 1
+	}
+
+	private void formatacao() {
+		System.out.println();
+	}
+}
