8bc0d055a049d635b3168546d0c758b6a8530dcb
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2014-04-05 14:08:59 -0300

    iterable frequency

--- guava-exemplos/src/main/java/collections/utilityClass/GuavaIterable.java
+++ guava-exemplos/src/main/java/collections/utilityClass/GuavaIterable.java
@@ -1,6 +1,8 @@
 package collections.utilityClass;
 
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
@@ -9,7 +11,8 @@ import java.util.List;
 public class GuavaIterable {
 
 	public static void main(String args[]) {
-		metodosEstaticos();
+//		metodosEstaticos();
+		frequency();
 	}
 
 	private static void metodosEstaticos() {
@@ -23,4 +26,36 @@ public class GuavaIterable {
 		System.out.println(Iterables.concat(list, list2));   //   [AAAAAA, BBBBB, CCCCC, AAAAAA, XXXX, ZZZZ]
 	}
 
+	private static void frequency() {
+	 	List<Produto> produtos = Lists.newArrayList(new Produto("Mouse", 1) ,
+				new Produto("Teclado", 1), new Produto("Notebook", 2));
+
+		System.out.println(FluentIterable.from(produtos).filter(new Predicate<Produto>() {
+			@Override
+			public boolean apply(Produto produto) {
+				return produto.getCategoria() == 1;
+			}
+		}).size());
+	}
+
+
+}
+
+class Produto {
+
+	private final String nome;
+	private final int categoria;
+
+	Produto(String nome, int categoria) {
+		this.nome = nome;
+		this.categoria = categoria;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getCategoria() {
+		return categoria;
+	}
 }
