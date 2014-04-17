098a233525f3a29a33c8970b47099e5da8542af9
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2014-04-17 17:23:47 -0300

    criado projeto java8

---
+++ java8/src/main/java/br/com/emmanuelneri/blog/classes/Pessoa.java
@@ -0,0 +1,24 @@
+package br.com.emmanuelneri.blog.classes;
+
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void acrecentarIdade() {
+        this.idade++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
