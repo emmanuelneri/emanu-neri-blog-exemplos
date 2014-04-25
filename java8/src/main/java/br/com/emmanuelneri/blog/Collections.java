11e2f291caa819e58a3ea682747bd42a6fe09170
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2014-04-25 00:42:15 -0300

    melhorias exemplos java8 lambda

--- java8/src/main/java/br/com/emmanuelneri/blog/Collections.java
+++ java8/src/main/java/br/com/emmanuelneri/blog/Collections.java
@@ -5,16 +5,19 @@ import br.com.emmanuelneri.blog.classes.Pessoa;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Collections {
 
     private static final List<Pessoa> pessoas = criarPessoas();
 
-    public static void main(String args[]) {
-    }
-
     private void forEach() {
+        Consumer<Pessoa> consumer = (Pessoa pessoa) -> System.out.println(pessoa.getNome());
+
+        pessoas.forEach(consumer);
         pessoas.forEach(pessoa -> System.out.println(pessoa.getNome()));
+
     }
 
     private void removeIf() {
@@ -23,6 +26,15 @@ public class Collections {
         lista.add(new Pessoa("Pessoa 2", 20));
         lista.add(new Pessoa("Pessoa 3", 23));
 
+        Predicate<Pessoa> predicate = new Predicate<Pessoa>() {
+            @Override
+            public boolean test(Pessoa pessoa) {
+                return pessoa.getIdade() == 23;
+            }
+        };
+        lista.removeIf(predicate);
+
+        //com Lambda
         lista.removeIf(p -> p.getIdade() == 20);
     }
 
