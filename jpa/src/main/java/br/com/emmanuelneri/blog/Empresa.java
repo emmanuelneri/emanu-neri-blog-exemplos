36bb84165e0a43359c8d46f43a7b2403a845dd04
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2014-07-28 00:02:22 -0300

    mapeando unique contraint

---
+++ jpa/src/main/java/br/com/emmanuelneri/blog/Empresa.java
@@ -0,0 +1,34 @@
+package br.com.emmanuelneri.blog;
+
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "CNPJ", name = "empresa_uk"))
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String cnpj;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
